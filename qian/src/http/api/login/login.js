957c31991edb77abcf9fdd9ce490c2be1d7f23ef
God-Y <q547114121@gmail.com>
2018-08-30 21:18:13 +0800

    ADD

--- qian/src/http/api/login/login.js
+++ qian/src/http/api/login/login.js
@@ -1,15 +1,5 @@
 // import axios from "../../http"; //导入创建好的axios实例
 
-<<<<<<< HEAD
-// export default {
-//   //登陆
-//   getList(data) {
-//     return  axios.get(`/api/a/list/bank`, {
-//       params: data
-//     });
-//   }
-// };
-=======
 export default {
   //登陆
   getList(data) {
@@ -18,4 +8,3 @@ export default {
     });
   }
 };
->>>>>>> bb16e0dda331691b9957522caec988e023bdfd14
